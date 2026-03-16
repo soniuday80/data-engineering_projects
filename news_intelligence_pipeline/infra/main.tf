30f49e2f3af250fa388114b74b2f0eb36e324ca3
uday <soniuday795@gmail.com>
2026-03-16 15:11:10 +0530

    -m "datawarehouse and database build will procced to load raw data"

--- news_intelligence_pipeline/infra/main.tf
+++ news_intelligence_pipeline/infra/main.tf
@@ -2,27 +2,37 @@ terraform {
   required_providers {
     snowflake = {
       source  = "snowflakedb/snowflake"
-      version = "~> 1.0" 
+      version = "~> 0.98" # changed the version
     }
   }
 }
 
 provider "snowflake" {
-    organization_name = var.snowflake_org
-    account_name      = var.snowflake_account
-    user              = "TF_SERVICEUSER"
-    role              = "SYSADMIN"
-    authenticator     = "SNOWFLAKE_JWT"
-    private_key       = file(var.snowflake_private_key_path)
+  alias   = "terraform"
+  organization_name = var.snowflake_org
+  account_name      = var.snowflake_account
+  user              = "TF_SERVICEUSER"
+  role              = "SYSADMIN"
+  authenticator     = "SNOWFLAKE_JWT"
+  private_key       = file(var.snowflake_private_key_path)
 }
 
-resource "snowflake_database" "news_db" {
-    name = "news_intelligence_db"
+resource "snowflake_database" "main" {
+  provider = snowflake.terraform
+  name     = "news_intelligence_db"
 }
 
-resource "snowflake_Warehouse" "news-wh"{
-    name           = "NEWS_INGESTION_WH"
-  warehouse_size = "X-SMALL"
+resource "snowflake_warehouse" "news_WH" {
+  provider       = snowflake.terraform
+  name           = "NEWS_INGESTION_WH"
+  warehouse_size = "xsmall"
   auto_suspend   = 60
-  auto_resume    = true
 }
+
+resource "snowflake_schema" "datawarehouse" {
+  provider   = snowflake.terraform
+  database   = snowflake_database.main.name
+  name       = "data_schema"
+}
+
+
